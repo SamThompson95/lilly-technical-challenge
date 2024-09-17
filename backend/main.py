e276af2fe045ea8c4d66087d53ee8120f49f7f4b
tpage-lilly <taylor.page@lilly.com>
2024-09-17 10:16:40 +0100

    Basic API

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
+import json
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/medicines")
 def read_root():
-    return {"Hello": "World"}
+    with open('data.json') as meds:
+        data = json.load(meds)
+    return data
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=3000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
