db07d78b6b7c23dc98dad8c9b10cc45786fa950e
tpage-lilly <taylor.page@lilly.com>
2024-09-27 15:21:45 +0100

    added missed step

--- start.sh
+++ start.sh
@@ -1,24 +1,29 @@
+
+#!/bin/bash
+
 # This script sets up and runs a Python virtual environment for the project.
 # 
 # Steps:
-# 1. Checks if the "venv" directory (virtual environment) does not exist.
-# 2. If the "venv" directory does not exist:
-#    a. Creates a new virtual environment in the "venv" directory.
+# 1. Checks if the "venv" directory exists.
+# 2. If "venv" does not exist:
+#    a. Creates a virtual environment named "venv".
 #    b. Activates the virtual environment.
 #    c. Installs the required Python packages from "requirements.txt".
-#    d. Runs the "main.py" script.
-# 3. If the "venv" directory already exists:
+#    d. Changes directory to "backend".
+#    e. Runs the "main.py" script.
+# 3. If "venv" exists:
 #    a. Activates the virtual environment.
-#    b. Runs the "main.py" script.
-#!/bin/bash
-
+#    b. Changes directory to "backend".
+#    c. Runs the "main.py" script.
 if [ ! -d "venv" ]; then
     python -m venv venv
     source venv/bin/activate
     pip install -r requirements.txt
+    cd backend
     python main.py
 fi
 else
     source venv/bin/activate
+    cd backend 
     python main.py
 fi
